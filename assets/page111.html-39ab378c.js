fe9cbb1f6af53a976b6e6f10cada28405f997811
binkyle <96855579+binkyle@users.noreply.github.com>
2023-11-03 11:50:47 +0000

    Deploying to gh-pages from @ binkyle/binkyle.github.io@0fbf5668ea1ce07f0f6723fb9a52c867b3dc68d4 🚀

---
+++ assets/page111.html-39ab378c.js
@@ -0,0 +1,13 @@
+import{_ as s}from"./plugin-vue_export-helper-c27b6911.js";import{r,o as h,c as n,a as o,b as e,d as t,e as l,f as i}from"./app-28d3ebb9.js";const p={},d=e("p",null,[e("code",null,"more"),t(" 注释之前的内容被视为文章摘要。")],-1),c=i('<h2 id="页面信息" tabindex="-1"><a class="header-anchor" href="#页面信息" aria-hidden="true">#</a> 页面信息</h2><p>你可以在 Markdown 的 Frontmatter 中设置页面信息。</p><ul><li>作者设置为 Ms.Hope。</li><li>写作日期为 2020 年 1 月 1 日</li><li>分类为 “使用指南”</li><li>标签为 “页面配置” 和 “使用指南”</li></ul><h2 id="页面内容" tabindex="-1"><a class="header-anchor" href="#页面内容" aria-hidden="true">#</a> 页面内容</h2><p>你可以自由在这里书写你的 Markdown。</p><div class="hint-container tip"><p class="hint-container-title">提示</p><ul><li><p>你可以将图片和 Markdown 文件放置在一起，但是你需要使用<strong>相对链接</strong><code>./</code> 进行引用。</p></li><li><p>对于 <code>.vuepress/public</code> 文件夹的图片，请使用绝对链接 <code>/</code> 进行引用。</p></li></ul></div><p>主题包含了一个自定义徽章可以使用:</p>',7),u=e("h2",{id:"页面结构",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#页面结构","aria-hidden":"true"},"#"),t(" 页面结构")],-1);function m(_,f){const a=r("Badge");return h(),n("div",null,[d,o(" more "),c,e("blockquote",null,[e("p",null,[t("文字结尾应该有深蓝色的 徽章文字 徽章。 "),l(a,{text:"徽章文字",color:"#242378"})])]),u,o(` 此页面应当包含:
+
+- [路径导航](https://theme-hope.vuejs.press/zh/guide/layout/breadcrumb.html)
+- [标题和页面信息](https://theme-hope.vuejs.press/zh/guide/feature/page-info.html)
+- [TOC (文章标题列表)](https://theme-hope.vuejs.press/zh/guide/layout/page.html#标题列表)
+- [贡献者、更新时间等页面元信息](https://theme-hope.vuejs.press/guide/feature/meta.html)
+- [评论](https://theme-hope.vuejs.press/zh/guide/feature/comment.html)
+- [导航栏](https://theme-hope.vuejs.press/zh/guide/layout/navbar.html)
+- [侧边栏](https://theme-hope.vuejs.press/zh/guide/layout/sidebar.html)
+- [页脚](https://theme-hope.vuejs.press/zh/guide/layout/footer.html)
+- 返回顶部按钮
+
+你可以通过主题选项和页面 Frontmatter 自定义它们。 `)])}const b=s(p,[["render",m],["__file","page111.html.vue"]]);export{b as default};
